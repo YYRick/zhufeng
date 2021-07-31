1a9ab2b7ff7cc823c1d869212745c6f264d6ee91
YYRick <2979434806@qq.com>
2021-07-31 18:19:32 +0800

    笔试题

---
+++ 1.js
@@ -0,0 +1,16 @@
+var person = { 
+     n:20,     
+     b:person.n*10
+    } 
+
+console.log(person.b);
+/* 
+  有一个数组：ary=[1,2,3,4,5,6,7,8,9,10]; 
+  把索引是3的倍数的项目依次打印出来
+*/
+/* var ary=[1,2,3,4,5,6,7,8,9,10]; 
+for( var i = 0;  i < ary.length; i++){
+    if((i >= 3)&&(i % 3 == 0)){
+        console.log(ary[i]);
+    }
+} */
